9d6594e10c58f6c083f2d57996e6b9612daa17eb
cpchia <chawchia@hotmail.co.uk>
2020-11-27 00:34:54 +0800

    Create privatekey & publickey & publicaddress

--- README.md
+++ README.md
@@ -22,3 +22,16 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### References:
+1) https://medium.com/blockthought/creating-bitcoin-wallets-in-js-69c0773c2954
+2) https://www.npmjs.com/package/elliptic-curve
+3) https://github.com/cryptocoinjs/bs58 
+4) https://www.npmjs.com/package/js-sha256
+5) https://github.com/niyas/bitcoin-address-generator //This library generate bitcoin address and random private key
+6) https://savjee.be/2018/10/Signing-transactions-blockchain-javascript/ Signing a transaction
+7) How to create privatekey/publickey/address/sign and verify message
+      // http://cryptocoinjs.com/modules/crypto/ecdsa/
+      // https://github.com/cryptocoinjs/coinkey
+      // http://cryptocoinjs.com/modules/crypto/ecdsa/#ecdsa 
--- src/components/CreateWallet.vue
+++ src/components/CreateWallet.vue
@@ -11,37 +11,72 @@
     </div>
 
     <form v-on:submit.prevent="GenerateWallet()">
-      <button type="submit" class="btn btn-primary">Generate Now</button><br><br>      
+      <button type="submit" class="btn btn-primary">Generate Now</button>
+      <br />
+      <br />
       <div class="form-group">
-        <textarea
+          <h3 class="lead">PRIVATE KEY</h3>
+          <textarea
           class="form-control"
-          rows="20"
-          cols="100"         
+          rows="2"
+          cols="100"
           id="GeneratedResult"
-          v-model="form.publicKey"
-        ></textarea>
-      </div>   
+          v-model="form.privateKey"></textarea><br>
+
+          <h3 class="lead">PUBLIC KEY</h3>
+          <textarea
+          class="form-control"
+          rows="2"
+          cols="100"
+          id="GeneratedResult"
+          v-model="form.publicKey"></textarea><br>
+
+          <h3 class="lead">PUBLIC ADDRESS</h3>
+          <textarea
+          class="form-control"
+          rows="2"
+          cols="100"
+          id="GeneratedResult"
+          v-model="form.address"></textarea><br>
+
+      </div>
     </form>
   </section>
 </template>
 
 <script>
-  export default {
-  name: 'GenerateWallet', 
-   data() {
-    return {      
+export default {
+  name: "GenerateWallet",
+  data() {
+    return {
       form: {
-        publicKey: "ABC12345689",       
-        privateKey: null,       
-        address:null
+        privateKey: null,
+        publicKey: null,        
+        address: null
       }
-    }
-   },
-   
-   methods: {   
+    };
+  },
+
+  methods: {
     GenerateWallet() {
-      console.log("Generating wallet now..");        
-      }
-   }
+      console.log("Generating wallet now..");
+
+      //The following refers to: http://cryptocoinjs.com/modules/crypto/ecdsa/
+      // and https://github.com/cryptocoinjs/coinkey
+      // and http://cryptocoinjs.com/modules/crypto/ecdsa/#ecdsa      
+      var sr = require("secure-random"); //npm install --save secure-random@1.x
+      var CoinKey = require("coinkey"); //npm install --save coinkey@0.1.0
+       
+      var privateKey = sr.randomBuffer(32);    
+      var ck = new CoinKey(privateKey, true); // true => compressed public key / addresses      
+      console.log("coinkey private key: "+ck.privateKey); //true
+      console.log("coinkey public key: "+ck.publicKey); //true
+      console.log("coinkey public address: "+ck.publicAddress); //true
+
+      this.form.privateKey=ck.privateKey.toString('hex');   
+      this.form.publicKey=ck.publicKey.toString('hex');        
+      this.form.address=ck.publicAddress.toString('hex');      
+    }
   }
+};
 </script>
