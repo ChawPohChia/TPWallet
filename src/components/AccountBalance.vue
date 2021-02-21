c67d5d5710a0d9a60cc1796653aecbf514191e3b
cpchia <chawchia@hotmail.co.uk>
2021-02-22 07:45:17 +0800

    Retrieve account balance using network api call

--- src/components/AccountBalance.vue
+++ src/components/AccountBalance.vue
@@ -10,21 +10,14 @@
       </div>
     </div>
 
-    <form v-on:submit.prevent="ViewWallet()">
+    <form v-on:submit.prevent="GetBalance()">
       <p class="lead">Wallet Address:</p>
-      <textarea class="form-control" rows="1" cols="100" id="walletAddress" v-model="form.walletAddress"></textarea><br><br>
+      <!--textarea class="form-control" rows="1" cols="100" id="walletAddress" v-model="form.walletAddress"></textarea><br><br-->
+      <p class="lead" style="color:red;font-weight:bold;font-size:30px">{{form.walletAddress}}</p> 
       <p class="lead">Blockchain Node To Connect:</p>
       <textarea class="form-control" rows="1" cols="100" id="nodeToConnect" v-model="form.nodeToConnect"></textarea><br><br>
       <button type="submit" class="btn btn-primary">Display Balance</button><br><br>      
-      <div class="form-group">
-        <textarea
-          class="form-control"
-          rows="20"
-          cols="100"         
-          id="DisplayedBalance"
-          v-model="form.balance"
-        ></textarea>
-      </div>   
+      <p class="lead" style="color:red;font-weight:bold;font-size:30px" id="balance"></p>   
     </form>
   </section>
 </template>
@@ -34,17 +27,35 @@
   name: 'OpenWallet', 
    data() {
     return {      
-      form: {
-        walletAddress: "This is wallet address",
-        nodeToConnect: "http://12.0.0.1/1234",       
-        balance: 100000
+      form: {       
+        walletAddress: "",
+        nodeToConnect: "http://12.0.0.1/1234", 
+        message:""
       }
     }
    },
-   
+
+  mounted() {
+      if (this.$store.state.publicAddress=="")
+            this.form.walletAddress="Open an existing wallet first to continue.";
+      else
+            this.form.walletAddress=this.$store.state.publicAddress;  
+  },
+
+     
    methods: {   
-    ViewWallet() {
-      console.log("Retrieving wallet balance now..");        
+    GetBalance() {
+      const axios = require("axios").default;
+      var accountBalanceApiCall="http://127.0.0.1:1234/address/"+this.$store.state.publicAddress+"/balance"
+      console.log(accountBalanceApiCall)
+      axios
+        .get(accountBalanceApiCall)
+        .then(function(response) {
+          if(response.data.balance==-1)
+              document.getElementById("balance").innerHTML  = response.data.message;              
+          else
+              document.getElementById("balance").innerHTML  = response.data.balance;  
+        });        
       }
    }
   }
