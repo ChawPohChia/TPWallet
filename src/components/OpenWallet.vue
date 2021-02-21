111deedf9db67125fada9580050989b1bba2f4a1
cpchia <chawchia@hotmail.co.uk>
2021-02-22 00:40:04 +0800

    Let user open wallet
    Wallet open using private key, and then persist public key and public address to store

--- src/components/OpenWallet.vue
+++ src/components/OpenWallet.vue
@@ -4,8 +4,8 @@
     <div class="row align-items-center jumbotron jumbotron-fluid page-header home full">
       <div class="col-md-12">
         <div class="container">
-          <h1 class="display-4">Open an Existing Wallet</h1>
-          <p class="lead">Enter your wallet private key (compressed ECDSA key, 65 hex digits):</p>
+          <h1 class="display-4">Open an Existing Wallet</h1>          
+          <p class="lead">Enter your private key to retrieve your wallet:</p>
         </div>
       </div>
     </div>
@@ -13,36 +13,60 @@
     <form v-on:submit.prevent="OpenWallet()">      
       <textarea class="form-control" rows="2" cols="100" id="keyedInPrivateKey" v-model="form.privateKey"></textarea><br><br>
       <button type="submit" class="btn btn-primary">Open Wallet</button><br><br>      
-      <div class="form-group">
-        <textarea
+      <div class="form-group">    
+         <h3 class="lead">PUBLIC KEY</h3>
+          <textarea
           class="form-control"
-          rows="20"
-          cols="100"         
+          rows="2"
+          cols="100"
           id="GeneratedResult"
-          v-model="form.publicKey"
-        ></textarea>
-      </div>   
+          v-model="form.publicKey"></textarea><br>
+
+          <h3 class="lead">PUBLIC ADDRESS</h3>
+          <textarea
+          class="form-control"
+          rows="2"
+          cols="100"
+          id="GeneratedResult"
+          v-model="form.address"></textarea><br>
+       </div>
     </form>
   </section>
 </template>
 
 <script>
+  import { ref } from "vue";
   export default {
-  name: 'OpemWallet', 
+  name: 'OpenWallet', 
+  
    data() {
     return {      
       form: {
-        publicKey: "ABC12345689",       
-        privateKey: "privatekeyabc",       
-        address:null
+        publicKey: ref(""),       
+        privateKey: ref(""),       
+        address: ref("")
       }
     }
    },
    
    methods: {   
     OpenWallet() {
-      console.log("Open existing wallet now..");        
-      }
-   }
-  }
+      var CoinKey = require('coinkey')      
+ 
+      var key = new CoinKey(new Buffer(this.form.privateKey, 'hex'))   
+      this.form.publicKey= key.publicKey.toString('hex'); 
+      this.form.address= key.publicAddress.toString('hex');     
+
+      this.$store.commit("persistPublicKey",this.form.publicKey);   
+      this.$store.commit("persistPublicAddress",this.form.address);  
+     
+      console.log(this.$store.state.publicKey);   
+      console.log(this.$store.state.publicAddress);   
+   
+   
+    }
+  },
+}
+  
 </script>
+
