35b2491a5269cb7dd1c2030dad365597b3f12e58
cpchia <chawchia@hotmail.co.uk>
2021-02-22 17:37:09 +0800

    Create wallet in blockchain
    Create wallet by adding wallet address into blockchain balances{} dictionary

--- src/components/CreateWallet.vue
+++ src/components/CreateWallet.vue
@@ -5,7 +5,9 @@
       <div class="col-md-12">
         <div class="container">
           <h1 class="display-4">Create a New Wallet</h1>
-          <p class="lead">Generate a new wallet: random private key -> public key -> address.</p>
+          <!--p class="lead">Generate a new wallet: random private key -> public key -> address.</p-->
+          <p class="lead" style="color:red;" id="message">Generate a new wallet: random private key -> public key -> address.</p>   
+          
         </div>
       </div>
     </div>
@@ -51,10 +53,10 @@ export default {
   data() {
     return {
       form: {
-        privateKey: null,
-        publicKey: null,        
-        address: null,
-        message: null
+        privateKey: "",
+        publicKey: "",        
+        address: "",
+        message: ""
       }
     };
   },
@@ -71,16 +73,34 @@ export default {
       var CoinKey = require("coinkey"); //npm install --save coinkey@0.1.0
        
       var privateKey = sr.randomBuffer(32);   
-      var ck = new CoinKey(privateKey)
+      var ck = new CoinKey(privateKey);
+      //var addressAcceptedByChain =false;
      
       console.log("coinkey private key: "+ck.privateKey); //true
       console.log("coinkey public key: "+ck.publicKey); //true
       console.log("coinkey public address: "+ck.publicAddress); //true
 
-      this.form.privateKey=ck.privateKey.toString('hex');   
-      this.form.publicKey=ck.publicKey.toString('hex');        
-      this.form.address=ck.publicAddress.toString('hex');  
-      this.form.message="Use your private key to retrieve your wallet. Please keep your private key in a safe place. Once you lost it, you lost your wallet forever!";      
+      const axios = require("axios").default;
+      var addAccountApiCall="http://127.0.0.1:1234/address/"+ck.publicAddress+"/add"
+      console.log(addAccountApiCall)
+      
+      let self=this;
+      const AddAccountServerReponse = function(data) {
+         console.log('accepted:', data.accepted)
+         console.log('message:', data.message)
+         document.getElementById("message").innerHTML = data.message;
+         if(data.accepted){
+           self.form.privateKey=ck.privateKey.toString('hex');   
+           self.form.publicKey=ck.publicKey.toString('hex');        
+           self.form.address=ck.publicAddress.toString('hex');  
+           self.form.message="Use your private key to retrieve your wallet. Please keep your private key in a safe place. Once you lost it, you lost your wallet forever!";     
+         }         
+      }
+      axios
+        .get(addAccountApiCall)
+        .then(function(response) { 
+             return response.data; })
+        .then(AddAccountServerReponse);       
     }
   }
 };
